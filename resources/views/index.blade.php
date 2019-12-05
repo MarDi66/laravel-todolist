d109d132c146e0d74ee2cd6b21234b4ad430dcb8
MarDi66 <yohann.mtfpro@gmail.com>
2019-12-05 14:57:33 +0100

    app.blade.php layout created + bootstrap linked

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,13 @@
-<h1>Todos</h1>
-@if (count($todos) > 0)
-    @foreach ($todos as $todo)
-        <div class="card">
-        <h2>{{ $todo->title }}</h2>
-        <h3>{{ $todo->content }}</h3>
-        <span class="label label-danger">{{ $todo->due }}</span>
-    </div>
-    @endforeach   
-@endif
+@extends('layouts.app')
+
+@section('content')
+    <h1>Todos</h1>
+    @if (count($todos) > 0)
+        @foreach ($todos as $todo)
+            <div class="card m-2">
+            <h2>{{ $todo->title }}</h2>
+            <span class="label label-danger">{{ $todo->due }}</span>
+        </div>
+        @endforeach   
+    @endif
+@endsection
